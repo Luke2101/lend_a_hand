c44b14cb5b36cffce34bd32e7f060a4587f3a7be
Luke2101 <94457209+Luke2101@users.noreply.github.com>
2025-10-14 12:40:18 +0200

    add CORS; add docker-compose.yml

--- api/src/api.ts
+++ api/src/api.ts
@@ -2,8 +2,15 @@ import express from "express";
 import 'dotenv/config'
 import userRoutes from "./routes/userRoutes.js";
 import {AuthHandler} from "./lib/AuthHandler.js";
+import cors from "cors";
 const app = express();
 const port = "8080";
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+}));
 
 app.use(express.json())
 app.use("/users", userRoutes)
