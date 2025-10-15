8f8bb33ee016acd037305f49c127fa5a6827463c
Marvin Kaiser <marvin.kaiser@bechtle.com>
2025-10-15 08:29:48 +0200

    fixed faulty commit

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -2,7 +2,7 @@ import express, {type Request} from "express";
 import {auth} from "../lib/auth.js";
 import {StatusCodes} from "http-status-codes";
 
-export async function authenticateUser(req: express.Request, res: express.Response, next: express.NextFunction): void {
+export async function authenticateUser(req: express.Request, res: express.Response, next: express.NextFunction): Promise<void> {
     const request = new Request("", {
         method: req.method,
         headers: new Headers(req.headers as Record<string, string>),
