431f7dfd23206c3bd5bc547312dc05e0a1ec554a
Marvin Kaiser <marvinkaiser@Air-von-Marvin.fritz.box>
2025-10-30 18:46:24 +0100

    Seesion cookies are cleared if they are invalid

--- api/src/middleware/authenticate.ts
+++ api/src/middleware/authenticate.ts
@@ -1,4 +1,4 @@
-import type {Response, Request} from "express";
+import type {Request, Response} from "express";
 import {auth} from "../lib/auth.js";
 import {StatusCodes} from "http-status-codes";
 import type {NextFunction} from "connect";
@@ -10,9 +10,18 @@ export async function authenticateUser(req: Request, res: Response, next: NextFu
     try {
         const session = await auth.api.getSession({headers});
         if(!session || !session.user) {
-            res.status(StatusCodes.UNAUTHORIZED).send("Ungültige Sitzung!");
+            const SESSION_COOKIE_NAME="better-auth.session_token";
+            res.clearCookie(SESSION_COOKIE_NAME, {
+                httpOnly: true,
+                sameSite: "lax",
+                path: "/",
+            }).status(StatusCodes.UNAUTHORIZED).send({message: "INVALID_SESSION"})
+
             return;
         }
+
+
+
         req.session = session.session;
         if(session.user.image === undefined) session.user.image = null;
         req.user = {
